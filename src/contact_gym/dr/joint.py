62e89e9b5d6864829795c819eaa217b79e12348d
Sherif-Sameh <sherif252525@gmail.com>
2026-08-21 14:58:22 +0300

    Add joint attribute DR module

---
+++ src/contact_gym/dr/joint.py
@@ -0,0 +1,134 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import mujoco
+
+from .base import ModelParamRandomizerCfg
+
+if TYPE_CHECKING:
+    from ..utils.noise import NoiseModel
+    from .base import ModelPostProc, SelectorType
+
+
+# region jnt_ attributes
+
+
+def joint_linear_stiffness_cfg(
+    noise: NoiseModel, jnt_sel: SelectorType, post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for joint linear stiffness randomizer configuration.
+
+    `jnt_sel` maps directly to `inst_sel` since attribute is defined per joint as `jnt_*`
+    arrays.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="jnt_stiffness", inst_sel=jnt_sel, post_proc=post_proc
+    )
+
+
+# region dof_ attributes
+
+
+def joint_dof_param_cfg(
+    model: mujoco.MjModel,
+    attr: str,
+    noise: NoiseModel,
+    jnt_sel: SelectorType,
+    attr_sel: SelectorType | None = None,
+    post_proc: ModelPostProc | None = None,
+) -> ModelParamRandomizerCfg:
+    """Factory for joint per-DOF generic parameter randomizer configuration.
+
+    `model` is required to map `jnt_sel` to `dof_sel` internally since attribute is
+    defined per DOF as `dof_*` arrays. `dof_sel` is passed as `inst_sel` to randomizer
+    configuration. `noise` should be configured with this conversion in mind for
+    multi-DOF joints.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    dof_sel = _jnt_sel_to_dof_sel(model, jnt_sel)
+    return ModelParamRandomizerCfg(
+        noise, attr=attr, inst_sel=dof_sel, attr_sel=attr_sel, post_proc=post_proc
+    )
+
+
+def joint_frictionloss_cfg(
+    model: mujoco.MjModel,
+    noise: NoiseModel,
+    jnt_sel: SelectorType,
+    post_proc: ModelPostProc | None = None,
+) -> ModelParamRandomizerCfg:
+    """Factory for joint friction-loss randomizer configuration.
+
+    **Important**: non-scalar `noise` should be configured with the joints' `dof`
+    dimension in mind, not the number of joints.
+
+    See :func:`joint_dof_param_cfg` for argument descriptions.
+    """
+    return joint_dof_param_cfg(model, "dof_frictionloss", noise, jnt_sel, post_proc=post_proc)
+
+
+def joint_armature_cfg(
+    model: mujoco.MjModel,
+    noise: NoiseModel,
+    jnt_sel: SelectorType,
+    post_proc: ModelPostProc | None = None,
+) -> ModelParamRandomizerCfg:
+    """Factory for joint armature randomizer configuration.
+
+    **Important**: non-scalar `noise` should be configured with the joints' `dof`
+    dimension in mind, not the number of joints.
+
+    See :func:`joint_dof_param_cfg` for argument descriptions.
+    """
+    return joint_dof_param_cfg(model, "dof_armature", noise, jnt_sel, post_proc=post_proc)
+
+
+def joint_linear_damping_cfg(
+    model: mujoco.MjModel,
+    noise: NoiseModel,
+    jnt_sel: SelectorType,
+    post_proc: ModelPostProc | None = None,
+) -> ModelParamRandomizerCfg:
+    """Factory for joint linear damping randomizer configuration.
+
+    **Important**: non-scalar `noise` should be configured with the joints' `dof`
+    dimension in mind, not the number of joints.
+
+    See :func:`joint_dof_param_cfg` for argument descriptions.
+    """
+    return joint_dof_param_cfg(model, "dof_damping", noise, jnt_sel, post_proc=post_proc)
+
+
+# region Helpers
+
+
+def _jnt_sel_to_dof_sel(model: mujoco.MjModel, jnt_sel: SelectorType) -> SelectorType:
+    if isinstance(jnt_sel, int):
+        dof_adr = model.jnt_dofadr[jnt_sel]
+        dof_dim = _get_joint_dof_dim(model.jnt_type[jnt_sel])
+        return dof_adr if dof_dim == 1 else slice(dof_adr, dof_adr + dof_dim)
+    if isinstance(jnt_sel, slice) and jnt_sel.step in [None, 1]:  # contiguous block
+        jnt_types = model.jnt_type[jnt_sel]
+        dof_adr = model.jnt_dofadr[0] if jnt_sel.start is None else model.jnt_dofadr[jnt_sel.start]
+        dof_dim = sum(_get_joint_dof_dim(jnt_type) for jnt_type in jnt_types)
+        return slice(dof_adr, dof_adr + dof_dim)
+    # Non-contiguous slice, sequence, or array of joints -> sequence of dofs
+    jnt_types = model.jnt_type[jnt_sel]
+    dof_adrs = model.jnt_dofadr[jnt_sel]
+    dof_dims = [_get_joint_dof_dim(jnt_type) for jnt_type in jnt_types]
+    return sum(
+        [tuple(range(dof_adr, dof_adr + dof_dim)) for dof_adr, dof_dim in zip(dof_adrs, dof_dims)],
+        start=(),
+    )
+
+
+def _get_joint_dof_dim(jnt_type: mujoco.mjtJoint) -> int:
+    if jnt_type == mujoco.mjtJoint.mjJNT_FREE:
+        return 6
+    if jnt_type == mujoco.mjtJoint.mjJNT_BALL:
+        return 3
+    return 1
