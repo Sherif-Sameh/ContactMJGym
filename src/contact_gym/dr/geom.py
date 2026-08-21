04ca43f52d4ccb71dd75d087e875b6fdecd96579
Sherif-Sameh <sherif252525@gmail.com>
2026-08-21 19:03:32 +0300

    Add geom attribute DR module

---
+++ src/contact_gym/dr/geom.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .base import ModelParamRandomizerCfg
+
+if TYPE_CHECKING:
+    from ..utils.noise import NoiseModel
+    from .base import ModelPostProc, SelectorType
+
+
+def geom_solref_cfg(
+    noise: NoiseModel, inst_sel: SelectorType = slice(None), post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for geom `solref` parameter randomizer configuration.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="geom_solref", inst_sel=inst_sel, attr_sel=slice(None), post_proc=post_proc
+    )
+
+
+def geom_solimp_impedance_cfg(
+    noise: NoiseModel, inst_sel: SelectorType = slice(None), post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for geom `solimp` impedance (`d0` and `dwidth`) parameter randomizer configuration.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="geom_solimp", inst_sel=inst_sel, attr_sel=slice(2), post_proc=post_proc
+    )
+
+
+def geom_sliding_friction_cfg(
+    noise: NoiseModel, inst_sel: SelectorType = slice(None), post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for geom sliding friction parameter randomizer configuration.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="geom_friction", inst_sel=inst_sel, attr_sel=0, post_proc=post_proc
+    )
+
+
+def geom_sliding_torsional_friction_cfg(
+    noise: NoiseModel, inst_sel: SelectorType = slice(None), post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for geom sliding and torsional friction parameter randomizer configuration.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="geom_friction", inst_sel=inst_sel, attr_sel=slice(2), post_proc=post_proc
+    )
+
+
+def geom_surface_linvel_cfg(
+    noise: NoiseModel, inst_sel: SelectorType = slice(None), post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for geom surface linear velocity parameter randomizer configuration.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="geom_surfacevel", inst_sel=inst_sel, attr_sel=slice(3), post_proc=post_proc
+    )
+
+
+def geom_surface_angvel_cfg(
+    noise: NoiseModel, inst_sel: SelectorType = slice(None), post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for geom surface angular velocity parameter randomizer configuration.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="geom_surfacevel", inst_sel=inst_sel, attr_sel=slice(3, 6), post_proc=post_proc
+    )
+
+
+def geom_adhesion_cfg(
+    noise: NoiseModel, inst_sel: SelectorType = slice(None), post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for geom adhesion parameter randomizer configuration.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="geom_adhesion", inst_sel=inst_sel, post_proc=post_proc
+    )
+
+
+def geom_rgb_cfg(
+    noise: NoiseModel, inst_sel: SelectorType = slice(None), post_proc: ModelPostProc | None = None
+) -> ModelParamRandomizerCfg:
+    """Factory for geom color parameter randomizer configuration.
+
+    See :class:`ModelParamRandomizerCfg` for argument descriptions.
+    """
+    return ModelParamRandomizerCfg(
+        noise, attr="geom_rgba", inst_sel=inst_sel, attr_sel=slice(3), post_proc=post_proc
+    )
