531802b6988c62bf55588c4afba08c87a6ea7b81
Sherif-Sameh <sherif252525@gmail.com>
2026-09-02 21:58:21 +0300

    Add global mjtJoint to qpos and dof dim lookup table to mj_utils. Update dr/base.py to use new lookup table

--- src/contact_gym/utils/mj_utils.py
+++ src/contact_gym/utils/mj_utils.py
@@ -1,5 +1,19 @@
 import mujoco
 
+MJTJOINT_TO_QPOS_NUM = {
+    mujoco.mjtJoint.mjJNT_FREE: 7,
+    mujoco.mjtJoint.mjJNT_BALL: 4,
+    mujoco.mjtJoint.mjJNT_SLIDE: 1,
+    mujoco.mjtJoint.mjJNT_HINGE: 1,
+}
+
+MJTJOINT_TO_DOF_DIM = {
+    mujoco.mjtJoint.mjJNT_FREE: 6,
+    mujoco.mjtJoint.mjJNT_BALL: 3,
+    mujoco.mjtJoint.mjJNT_SLIDE: 1,
+    mujoco.mjtJoint.mjJNT_HINGE: 1,
+}
+
 
 def get_dof_dim_from_joints(model: mujoco.MjModel, qpos_adr: int, qpos_dim: int = 1) -> int:
     """Get the `dof` dimension of a set of joints according to their types.
